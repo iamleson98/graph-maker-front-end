65f446b63468e9bc2a75932d8051ebb4fa5a7b40
leminhson2398 <leminhson2398@outlook.com>
2020-10-26 23:47:13 +0700

    commit

--- src/__fixtures__/chart/standardChart/scatter.tsx
+++ src/__fixtures__/chart/standardChart/scatter.tsx
@@ -1,9 +1,44 @@
+import { ChartConfiguration } from "chart.js"
 import React, { memo } from "react"
+import CommonStdChart from "./CommonStandard"
 
 
-function ScatterStdChart() {
+export interface StdScatterChartProps {
+    name?: string;
+}
+
+export function scatterChartConfig({ name }: StdScatterChartProps): ChartConfiguration {
+    return {
+        type: "scatter",
+        options: {
+            responsive: true,
+            legend: {
+                align: "start",
+                position: "bottom",
+                fullWidth: false,
+                labels: {
+                    boxWidth: 12,
+                }
+            },
+            animation: {
+                duration: 500,
+                easing: "linear",
+                animateScale: true,
+                animateRotate: true
+            },
+            title: {
+                display: true,
+                text: name || "Scatter chart"
+            }
+        }
+    }
+}
+
+function ScatterStdChart(props: StdScatterChartProps) {
     return (
-        <div></div>
+        <CommonStdChart
+            config={scatterChartConfig(props)}
+        />
     )
 }
 
